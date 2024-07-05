fcc488ff535562764f91c20b03da469d3e4e4b03
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-07-05 00:26:38 -0300

    Create income_category and expense_category model

--- wallet/categories/models.py
+++ wallet/categories/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import CustomUser
+
+
+class IncomeCategory(models.Model):
+    """
+    Represents an income category.
+
+    Attributes:
+        name (str): The name of the income category.
+        is_default (bool): Indicates whether the income category is a default category.
+        user (CustomUser): The user associated with the income category.
+
+    Meta:
+
+        db_table (str): Specifies the name of the database table ('income_categories').
+
+    Methods:
+        __str__(): Returns a string representation of the income category.
+    """
+
+    name = models.CharField(max_length=50)
+    is_default = models.BooleanField(default=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta():
+        db_table = 'income_categories'
+
+    def __str__(self) -> str:
+        if self.is_default:
+            return f"IncomeCategory(ID: {self.id}, Name: {self.name}, Default: {self.is_default})"
+        return f"IncomeCategory(ID: {self.id}, Name: {self.name}, User: {self.user})"
+
+
+class ExpenseCategory(models.Model):
+    """
+    Represents an expense category.
+
+    Attributes:
+        name (str): The name of the expense category.
+        is_default (bool): Indicates whether the category is a default category.
+        user (CustomUser): The user associated with the category.
+
+    Meta:
+        db_table (str): Specifies the name of the database table ('expense_categories').
+
+    Methods:
+        __str__(): Returns a string representation of the expense category.
+    """
+
+    name = models.CharField(max_length=50)
+    is_default = models.BooleanField(default=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta():
+        db_table = 'expense_categories'
+
+    def __str__(self) -> str:
+        if self.is_default:
+            return f"ExpenseCategory(ID: {self.id}, Name: {self.name}, Default: {self.is_default})"
+        return f"ExpenseCategory(ID: {self.id}, Name: {self.name}, User: {self.user})"
