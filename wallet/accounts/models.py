35715253dae7e062bf00e4b70a3a75c5b35fa274
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-07-05 00:26:12 -0300

    Create account model

--- wallet/accounts/models.py
+++ wallet/accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import CustomUser
+
+
+class Account(models.Model):
+    """
+    Represents a user's account.
+
+    Attributes:
+        balance (DecimalField): The current balance of the account.
+        name (CharField): The name of the account.
+        user (ForeignKey): Reference to the CustomUser who owns the account.
+
+    Meta:
+        db_table: Specifies the name of the database table ('accounts').
+
+    Methods:
+        __str__: Returns a string representation of the account.
+    """    
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta():
+        db_table = 'accounts'
+
+    def __str__(self):
+        return f'Account(ID: {self.id}, Name: {self.name}, User: {self.user})'
