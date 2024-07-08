0fb7286b789d8603e5852e6d795767de2094419f
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-07-08 20:48:07 -0300

    Define login url and auth user model in project settings

--- wallet/wallet/settings.py
+++ wallet/wallet/settings.py
@@ -13,7 +13,7 @@ import os
 import environ
 from pathlib import Path
 
-env = environ.Env(DEBUG=(bool, False))   
+env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +143,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.CustomUser'
+
+LOGIN_URL = 'login'
