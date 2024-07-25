1d05240d5645dde8f75cde4d55242b281686f700
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-07-24 23:28:11 -0300

    Create view for CustomUser API

--- wallet/api/views.py
+++ wallet/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from authentication.models import CustomUser
+from authentication.serializers import CustomUserSerializer
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
