b12c3f847664e69c774d995c49dc7ea45f06679f
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-07-24 23:27:50 -0300

    Create serializer for CustomUser model

---
+++ wallet/authentication/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from .models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the CustomUser model.
+    """
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'first_name', 'last_name', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
