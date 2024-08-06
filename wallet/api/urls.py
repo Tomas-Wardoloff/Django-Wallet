65e0bebddf8aaacf7b481bcd0a73cf56ddce7388
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-08-06 11:41:14 -0300

    Create view and urls for Account API

--- wallet/api/urls.py
+++ wallet/api/urls.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 
-from .views import CustomUserViewSet
+from .views import CustomUserViewSet, AccountViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'users', CustomUserViewSet)
+#router.register(r'expenses', ExpenseViewSet)
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = router.urls
--- wallet/api/views.py
+++ wallet/api/views.py
@@ -1,9 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from authentication.models import CustomUser
 from authentication.serializers import CustomUserSerializer
+from accounts.models import Account
+from accounts.serializers import AccountSerializer
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
