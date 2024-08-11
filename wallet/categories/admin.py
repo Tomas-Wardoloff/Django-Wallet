e44eb3a307aca81cae0834d4f7892524b96f5689
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-08-11 12:38:53 -0300

    Add category to admin page

--- wallet/categories/admin.py
+++ wallet/categories/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+class DefaultCategoryFilter(admin.SimpleListFilter):
+    title = 'default category'
+    parameter_name = 'default'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.filter(user__isnull=True)
+        if self.value() == 'no':
+            return queryset.filter(user__isnull=False)
+        return queryset
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_filter = ['type', DefaultCategoryFilter]
+    list_display = ['name', 'type', 'user']
+
+
+admin.site.register(Category, CategoryAdmin)
