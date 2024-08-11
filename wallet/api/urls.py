c322876fda0ae8cd4761a4bc5efa7e1b8abaf5ce
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-08-11 13:25:42 -0300

    Create view and urls for Category API

--- wallet/api/urls.py
+++ wallet/api/urls.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 
-from .views import CustomUserViewSet, AccountViewSet
-
+from .views import (
+    CustomUserViewSet,
+    AccountViewSet,
+    CategoryViewSet)
 
 router = routers.DefaultRouter()
 router.register(r'users', CustomUserViewSet)
-#router.register(r'expenses', ExpenseViewSet)
 router.register(r'accounts', AccountViewSet)
+router.register(r'categories', CategoryViewSet)
 
 urlpatterns = router.urls
--- wallet/api/views.py
+++ wallet/api/views.py
@@ -1,10 +1,16 @@
+from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 
 from authentication.models import CustomUser
 from authentication.serializers import CustomUserSerializer
 from accounts.models import Account
 from accounts.serializers import AccountSerializer
+from transactions.models import Transfer, Transaction
+from transactions.serializers import TransactionSerializer, TransferSerializer
+from categories.models import Category
+from categories.serializers import CategorySerializer
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
@@ -22,3 +28,20 @@ class AccountViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Account.objects.filter(user=self.request.user)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Category.objects.filter(Q(user=self.request.user) | Q(user__isnull=True))
+
+    def perform_destroy(self, instance):
+        if instance.user != self.request.user:
+            raise PermissionDenied(
+                "User does not have permission to delete the category")
