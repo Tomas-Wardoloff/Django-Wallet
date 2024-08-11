0432d74d001528df665e75638abfb2856710ce7d
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-08-11 12:41:41 -0300

    Add transaction to admin site

--- wallet/transactions/admin.py
+++ wallet/transactions/admin.py
@@ -1,24 +1,18 @@
 from django.contrib import admin
 
-from .models import Income, Expense, Transfer
+from .models import Transaction, Transfer
 
 
-class IncomeAdmin(admin.ModelAdmin):
-    model = Income
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
     list_filter = ['date', 'category', 'user', 'user_account']
-    list_display = ['amount', 'date', 'category', 'user', 'user_account_name']
+    list_display = ['amount', 'date', 'category', 'type', 'user', 'user_account_name']
 
     def user_account_name(self, obj) -> str:
         return obj.user_account.name
-
-
-class ExpenseAdmin(admin.ModelAdmin):
-    model = Expense
-    list_filter = ['date', 'category', 'user', 'user_account']
-    list_display = ['amount', 'date', 'category', 'user', 'user_account_name']
-
-    def user_account_name(self, obj) -> str:
-        return obj.user_account.name
+    
+    def type(self, obj) -> str:
+        return obj.category.type
 
 
 class TransferAmind(admin.ModelAdmin):
@@ -34,6 +28,5 @@ class TransferAmind(admin.ModelAdmin):
         return obj.to_user_account.name
 
 
-admin.site.register(Income, IncomeAdmin)
-admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Transfer)
