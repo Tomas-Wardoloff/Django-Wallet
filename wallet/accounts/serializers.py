540b0ac264687031353a4c93d03176051947f794
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-08-12 12:57:20 -0300

    Implement UserEmailSerializer in AccountSerializer

--- wallet/accounts/serializers.py
+++ wallet/accounts/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
 from .models import Account
+from authentication.serializers import UserEmailSerializer
 
 
 class AccountSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.id')
+    user = UserEmailSerializer(read_only=True)
 
     class Meta:
         model = Account
