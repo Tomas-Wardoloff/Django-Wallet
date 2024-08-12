8b2fd2735bf94b03f3c416ecf679a9552d9eac90
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-08-12 13:18:03 -0300

    Check category is default or belong to the user

--- wallet/transactions/models.py
+++ wallet/transactions/models.py
@@ -18,14 +18,15 @@ class Transaction(models.Model):
         user_account: (ForeignKey to Account): Refrence to the Account where the income was received.
         category: (ForeignKey to Category): The category of the transaction, linked to Category.
         description: (CharField): Optional description of the income.
-        
+
     Meta:
         db_table: Specifies the name of the database table ('incomes').
 
     Methods:
         __str__: Returns a string representation of the income.
     """
-    amount = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    amount = MoneyField(max_digits=10, decimal_places=2,
+                        default_currency='USD')
     date = models.DateField()
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     user_account = models.ForeignKey(Account, on_delete=models.CASCADE)
@@ -39,12 +40,13 @@ class Transaction(models.Model):
         if self.category.type == 'Income':
             return f'Income(ID: {self.id}, Amount: {self.amount}, User: {self.user}, Date: {self.date})'
         return f'Expense(ID: {self.id}, Amount: {self.amount}, User: {self.user}, Date: {self.date})'
-        
-        
+
     def clean(self):
         if self.user != self.user_account.user:
             raise ValidationError("The account does not belong to the user")
-    
+        if self.category.user and self.user != self.category.user:
+            raise ValidationError("The category does not belong to the user")
+
     def save(self, *args, **kwargs):
         self.clean()
         super().save(*args, **kwargs)
@@ -60,14 +62,15 @@ class Transfer(models.Model):
         from_user_account: (ForeignKey to Account): Reference to the Account where the money was transferred from.
         to_user_account: (ForeignKey to Account): Reference to the Account where the money was transferred to.
         description: (CharField): Optional description of the transfer.
-        
+
     Meta:
         db_table: Specifies the name of the database table ('transfers').
 
     Methods:
         __str__: Returns a string representation of the transfer.
     """
-    amount = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    amount = MoneyField(max_digits=10, decimal_places=2,
+                        default_currency='USD')
     date = models.DateField()
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     from_user_account = models.ForeignKey(
@@ -84,7 +87,8 @@ class Transfer(models.Model):
 
     def clean(self):
         if self.user != self.from_user_account.user or self.user != self.to_user_account.user:
-            raise ValidationError("The accounts involved do not belong to the user")
+            raise ValidationError(
+                "The accounts involved do not belong to the user")
 
     def save(self, *args, **kwargs):
         self.clean()
