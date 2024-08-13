26b3cd4e5cb955c310a1240affc33fb92440e712
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-08-13 16:10:33 -0300

    Create serializer to view and create Transactions

--- wallet/transactions/serializers.py
+++ wallet/transactions/serializers.py
@@ -1,30 +1,47 @@
+from django.core.exceptions import ValidationError
+
 from rest_framework import serializers
 
 from .models import Transaction, Transfer
 
 
-class TransactionSerializer(serializers.ModelSerializer):
-    user_email = serializers.SerializerMethodField()
-    account_name = serializers.SerializerMethodField()
-    category_name = serializers.SerializerMethodField()
-    type = serializers.SerializerMethodField()
+class TransactionDetailSerializer(serializers.ModelSerializer):
+    user = serializers.EmailField(source='user.email', read_only=True)
+    user_account = serializers.CharField(source='user_account.name')
+    category = serializers.SerializerMethodField()
 
     class Meta:
         model = Transaction
-        fields = ['id',  'user_email', 'amount', 'date',
-                  'account_name', 'category_name', 'type', 'description']
+        fields = ['id', 'amount', 'date', 'user',
+                  'user_account', 'category', 'description']
 
-    def get_user_email(self, obj):
-        return obj.user.email
+    def get_category(self, obj):
+        return {
+            'name': obj.category.name,
+            'type': obj.category.type
+        }
 
-    def get_account_name(self, obj):
-        return obj.user_account.name
 
-    def get_category_name(self, obj):
-        return obj.category.name
+class TransactionCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['amount', 'date', 'user_account', 'category', 'description']
 
-    def get_type(self, obj):
-        return obj.category.type
+    def validate(self, data):
+        user = self.context['request'].user
+        account = data['user_account']
+        category = data['category']
+
+        if account.user != user:
+            raise ValidationError(
+                {'user_account': 'The account does not belong to the user'})
+
+        if category.user and user != category.user:
+            raise ValidationError(
+                {'category': 'The category is not a default category or does not belong to the user'})
+
+        return data
+
 
 class TransferSerializer(serializers.ModelSerializer):
     user_email = serializers.SerializerMethodField()
